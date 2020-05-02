7c8806e774ed9f3a8bd64e47237862fa3c8f4598
lattrellsapon <lattrellsapon@gmail.com>
2020-05-02 15:51:25 +1200

    Version 1.0.0

--- server.js
+++ server.js
@@ -21,6 +21,15 @@ app.use(express.json());
 // Routes
 app.use('/api/prayer-intentions', prayerIntentions);
 
+// Serve static asserts if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set Statoc Folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // PORT
 const PORT = process.env.PORT || 5000;
 
