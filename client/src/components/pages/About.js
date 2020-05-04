a9be7aeb3a94d581bdd27804efbd26e2024a3ce7
lattrellsapon <lattrellsapon@gmail.com>
2020-05-04 17:31:08 +1200

    Added the about page

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -5,7 +5,31 @@ export const About = () => {
     <div className='min-height about-header'>
       <div className='about-header text-center'>
         <h1>About</h1>
-        <p>lorem10</p>
+      </div>
+      <div className='about-content'>
+        <p>
+          There is no doubt that this global pandemic has not only turned our
+          world upside down but has threatened the stability of our own faith.
+          But what we may have come to realise is that through this trial God
+          has given us more opportunities to pray, whether it be on our own,
+          with our families, or even virtually with our friends.
+        </p>
+        <p>
+          There is so much noise around us that hinder us from hearing God’s
+          voice. But this lockdown has silenced them, allowing us to hear Him so
+          much more clearly. Although our usual avenues for spiritual
+          nourishment have been taken away, may it not discourage us but instead
+          awaken our spiritual hunger.
+        </p>
+        <p>
+          We may have been on different journeys, but it has all led us to this
+          point. It doesn’t matter if this lockdown has allowed us to thrive or
+          caused us great struggle because God continues to call us to Him. So,
+          brothers and sisters I urge you to answer His calling to seek Him even
+          more fiercely. After lockdown the noise of the world will return so we
+          have to stay vigilant, anchor ourselves in prayer and continue to seek
+          God’s voice amidst all the distractions.
+        </p>
       </div>
     </div>
   );
